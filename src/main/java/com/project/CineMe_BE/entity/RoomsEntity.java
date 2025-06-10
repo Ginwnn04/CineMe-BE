cce2fd428b34eae743af59f615697d986f63698f
TrungHung07 <nguyenvotrunghung2004@gmail.com>
2025-06-10 21:51:29 +0700

    fix typo

--- src/main/java/com/project/CineMe_BE/entity/RoomsEntity.java
+++ src/main/java/com/project/CineMe_BE/entity/RoomsEntity.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import java.util.List;
 
 @Entity
-@Table(name = "movies")
+@Table(name = "rooms")
 @Setter
 @Getter
 @Builder
--- src/main/java/com/project/CineMe_BE/response/APIResponse.java
+++ src/main/java/com/project/CineMe_BE/response/APIResponse.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class APIResponse {
     @Builder.Default
-    private boolean sucess = true;
+    private boolean success = true;
     private Integer code;
     private String message;
     private Object result;
@@ -25,23 +25,8 @@ public class APIResponse {
     public static APIResponse ok(Object result) {
         return APIResponse.builder()
                           .code(200)
-                          .sucess(true)
+                          .success(true)
                           .result(result)
-                          .build();
-    }
-
-    /**
-     * Generate APIResponse with code 200
-     * @param result
-     * @param message
-     * @return
-     */
-    public static APIResponse ok(Object result, String message) {
-        return APIResponse.builder()
-                          .code(200)
-                          .sucess(true)
-                          .result(result)
-                          .message(message)
                           .build();
     }
 }
