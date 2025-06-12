4fd8036fa07bd95c55395e665eacb9d03b085c7e
Nguyen Nhat Quang <63640626+Ginwnn04@users.noreply.github.com>
2025-06-12 22:34:34 +0700

    Update src/main/java/com/project/CineMe_BE/utils/DomainUtil.java
    fix static when use @Value
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/main/java/com/project/CineMe_BE/utils/DomainUtil.java
+++ src/main/java/com/project/CineMe_BE/utils/DomainUtil.java
@@ -2,7 +2,21 @@ package com.project.CineMe_BE.utils;
 
 import org.springframework.beans.factory.annotation.Value;
 
+package com.project.CineMe_BE.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DomainUtil {
-    @Value("${MINIO_URL}")
-    public static String MINIO_DOMAIN;
+
+    private static String MINIO_DOMAIN;
+
+    public DomainUtil(@Value("${MINIO_URL}") String domain) {
+        MINIO_DOMAIN = domain;
+    }
+
+    public static String getMinioDomain() {
+        return MINIO_DOMAIN;
+    }
 }
