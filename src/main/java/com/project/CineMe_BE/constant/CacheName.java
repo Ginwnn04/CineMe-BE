f3ac1bcf44dc12e36ea85b7b837e93f601eda078
TrungHung07 <nguyenvotrunghung2004@gmail.com>
2025-07-17 15:46:26 +0700

    feat : done crud of permission

--- src/main/java/com/project/CineMe_BE/constant/CacheName.java
+++ src/main/java/com/project/CineMe_BE/constant/CacheName.java
@@ -4,4 +4,5 @@ public class CacheName {
     public static final String MOVIE = "movie";
     public static final String SEAT = "seat";
     public static final String ROLE = "role";
+    public static final String PERMISSION = "permission";
 }
